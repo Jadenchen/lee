cf9f7be6912cab43371fa50a1850dbcd11b18600
Jaden.Chen <jaden.chen@vatics.com>
2022-12-02 10:31:38 +0800

    leetcode: refine 704

--- binarysearch/704.c
+++ binarysearch/704.c
@@ -1,35 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#if 0
-int binsearch(int *nums, int target, int low, int high)
-{
-    int res = -1;
-    int mid = 0;
-    while(low != high) {
-        mid = (low + high)/2;
-        if (nums[mid] == target) {
-            res = mid;
-            break;
-        } else if (nums[mid] > target)
-            high = high - 1;
-        else
-            low = low + 1;
-    }
-    return res;
-}
-#endif
 
 int search(int *nums, int numsSize, int target)
 {
 	int ret = -1;
 	int mid = 0;
 	int low = 0;
-	int high = numsSize;
+	int high = numsSize - 1;
 	if (!numsSize)
 		return ret;
 
-	while(low != high) {
+	while(low <= high) {
 		mid = (low + high)/2;
 		if (nums[mid] == target) {
 			ret = mid;
@@ -42,11 +24,25 @@ int search(int *nums, int numsSize, int target)
 	return ret;
 }
 
+int search_recu(int *nums, int low, int high, int target)
+{
+	int mid;
+	if (low > high)
+		return -1;
+	mid = (low + high) >> 1;
+	if (nums[mid] == target)
+		return mid;
+	else if (nums[mid] > target)
+		search_recu(nums, low, high - 1, target);
+	else 
+		search_recu(nums, low + 1, high, target);
+}
+
 int main(void)
 {
 	int nums[6] = {-1,0,3,5,9,12};
 	int target = 9;
 	printf("index %d \n", search(nums, sizeof(nums)/sizeof(int), target));
-	printf("%d ", 5/2);
+	printf("index %d \n", search_recu(nums, 0, sizeof(nums)/sizeof(int), target));
 	return 0;
 }
