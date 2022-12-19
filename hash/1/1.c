eac4aaa1ebbf8dfbb60256fd28989ae7f18921d1
Jaden.Chen <jaden.chen@vatics.com>
2022-12-19 18:34:24 +0800

    hash: 1: add hasmap algo

--- hash/1/1.c
+++ hash/1/1.c
@@ -1,5 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE (10)
+#define HASH(key) ((key)*(key) % (SIZE))
+
+//! hash map inplement
+typedef struct hash {
+	int key;
+	struct hash *next;
+} hash;
+
+static hash g_tHash[SIZE];
+
+static void init_hash(int *arr, int len)
+{
+	int index = 0;
+	memset(&g_tHash[0], 0, SIZE*sizeof(hash));
+	for (size_t i = 0; i < len; i++) {
+		index = HASH(arr[i]);
+		printf("index %d \n", index);
+	}
+}
+
+static void release_hash(void)
+{
+	hash *curr = NULL;
+	hash *remove = NULL;
+	for (size_t i = 0; i < SIZE; i++) {
+		curr = &g_tHash[i];
+		while(curr) {
+			remove = curr;
+			curr = curr->next;
+			free(remove);
+		}	
+	}
+}
+
+static hash* new_node(int key)
+{
+	hash *new = calloc(1, sizeof(hash));
+	if (!new)
+		return NULL;
+	new->key = key;
+	new->next = NULL;
+}
+
+static void add_hash(int key, int index)
+{
+	hash *curr = &g_tHash[index];
+	hash *new = new_node(key);
+	while(curr->next) {
+		curr = curr->next;
+	}
+	
+	curr->next = new;
+}
+
+static void show_hash(void)
+{
+	hash *curr = NULL;
+	for (size_t i = 0; i < SIZE; i++) {
+		curr  = &g_tHash[i];	
+		while(curr) {
+			printf("%d ", curr->key);
+			curr = curr->next;
+		}
+		printf("\n");
+	}
+}
 
 int *two_sum(int *arr, int len, int target, int *returnSize)
 {
@@ -33,9 +100,11 @@ int main(void)
 	int returnSize = 0;
 	int len = sizeof(arr)/sizeof(int);
 	int *result = NULL;
+#if 0
 	result = two_sum(arr, len, target, &returnSize);
 	for(size_t i = 0; i < returnSize; i++) 
 		printf("%d ", result[i]);
+#endif
 	result = two_sum_mdi(arr, len, target, &returnSize);
 	for(size_t i = 0; i < returnSize; i++) 
 		printf("%d ", result[i]);
