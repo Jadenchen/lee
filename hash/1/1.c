425542afd5add28442329398c9587d21df3b706e
Jadenchen <menzxisland@gmail.com>
2022-12-20 08:37:26 +0800

    hash: 1.c: add hash algo

--- hash/1/1.c
+++ hash/1/1.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE (10)
+#define SIZE (4)
 #define HASH(key) ((key)*(key) % (SIZE))
 
 //! hash map inplement
 typedef struct hash {
 	int key;
+	int index;
 	struct hash *next;
 } hash;
 
-static hash g_tHash[SIZE];
+static hash *g_ptHash[SIZE] = {NULL};
 
-static void init_hash(int *arr, int len)
+static hash* new_node(int key)
 {
-	int index = 0;
-	memset(&g_tHash[0], 0, SIZE*sizeof(hash));
-	for (size_t i = 0; i < len; i++) {
-		index = HASH(arr[i]);
-		printf("index %d \n", index);
+	hash *new = calloc(1, sizeof(hash));
+	if (!new)
+		return NULL;
+	new->key = key;
+	new->next = NULL;
+}
+
+static void add_hash(int key, int index)
+{
+	int hasnum = HASH(key);
+	hash **curr = &g_tHash[hasnum];
+	hash *new = new_node(key, index);
+	while(*curr) {
+		curr = &(*curr)->next;
 	}
+	
+	curr->next = new;
 }
 
 static void release_hash(void)
@@ -35,25 +47,6 @@ static void release_hash(void)
 	}
 }
 
-static hash* new_node(int key)
-{
-	hash *new = calloc(1, sizeof(hash));
-	if (!new)
-		return NULL;
-	new->key = key;
-	new->next = NULL;
-}
-
-static void add_hash(int key, int index)
-{
-	hash *curr = &g_tHash[index];
-	hash *new = new_node(key);
-	while(curr->next) {
-		curr = curr->next;
-	}
-	
-	curr->next = new;
-}
 
 static void show_hash(void)
 {
@@ -85,11 +78,44 @@ int *two_sum(int *arr, int len, int target, int *returnSize)
 	return result;
 }
 
+static int haskey(int key) 
+{
+	hash **curr = NULL;
+	int index = HASH(key);
+	printf("index %d key %d \n", index, key);
+
+	curr = &g_ptHash[index];
+	while(*curr) {
+		if (curr->key == key)
+			break;
+
+		curr = &(*curr)->next;
+	}
+
+	return curr->index;
+}
+
 int *two_sum_mdi(int *arr, int len, int target, int *returnSize)
 {
 	int *result = NULL;
 	*returnSize = 2;
+	int x = 0;
 	result = calloc(2, sizeof(int));
+
+	for (size_t i = 0; i < len; i++) {
+		x = target - arr[i];
+		printf("x %d arr %d \n", x, arr[i]);
+		if (haskey(x)) {
+			//result[0] = i;
+			//result[1] = 
+		}
+		add_hash(arr[i], i);
+	}	
+
+	show_hash();
+
+
+	release_hash();
 	return result;
 }
 
