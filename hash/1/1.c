927c8fa2219802a6237ecace7abedf7e927c1b79
Jadenchen <menzxisland@gmail.com>
2022-12-21 00:10:31 +0800

    hash: 1: finish hash algo

--- hash/1/1.c
+++ hash/1/1.c
@@ -4,6 +4,7 @@
 #define HASH(key) ((key)*(key) % (SIZE))
 
 //! hash map inplement
+#if 0
 typedef struct hash {
 	int key;
 	int index;
@@ -12,25 +13,25 @@ typedef struct hash {
 
 static hash *g_ptHash[SIZE] = {NULL};
 
-static hash* new_node(int key)
+static hash* new_node(int key, int index)
 {
 	hash *new = calloc(1, sizeof(hash));
 	if (!new)
 		return NULL;
 	new->key = key;
+	new->index = index;
 	new->next = NULL;
 }
 
 static void add_hash(int key, int index)
 {
 	int hasnum = HASH(key);
-	hash **curr = &g_tHash[hasnum];
+	hash **curr = &g_ptHash[hasnum];
 	hash *new = new_node(key, index);
-	while(*curr) {
+	while(*curr)
 		curr = &(*curr)->next;
-	}
-	
-	curr->next = new;
+
+	*curr = new;
 }
 
 static void release_hash(void)
@@ -38,7 +39,7 @@ static void release_hash(void)
 	hash *curr = NULL;
 	hash *remove = NULL;
 	for (size_t i = 0; i < SIZE; i++) {
-		curr = &g_tHash[i];
+		curr = g_ptHash[i];
 		while(curr) {
 			remove = curr;
 			curr = curr->next;
@@ -47,12 +48,11 @@ static void release_hash(void)
 	}
 }
 
-
 static void show_hash(void)
 {
 	hash *curr = NULL;
 	for (size_t i = 0; i < SIZE; i++) {
-		curr  = &g_tHash[i];	
+		curr  = g_ptHash[i];	
 		while(curr) {
 			printf("%d ", curr->key);
 			curr = curr->next;
@@ -74,25 +74,25 @@ int *two_sum(int *arr, int len, int target, int *returnSize)
 			}
 		}
 	}
-	
+
 	return result;
 }
 
-static int haskey(int key) 
+static int haskey(int key, int *index) 
 {
-	hash **curr = NULL;
-	int index = HASH(key);
-	printf("index %d key %d \n", index, key);
-
-	curr = &g_ptHash[index];
-	while(*curr) {
-		if (curr->key == key)
+	int bKey = 0;
+	int hashnum = HASH(key);
+	hash *curr = g_ptHash[hashnum];
+	while(curr) {
+		if (curr->key == key) {
+			bKey = 1;
+			*index = curr->index;
 			break;
-
-		curr = &(*curr)->next;
+		}
+		curr = curr->next;
 	}
 
-	return curr->index;
+	return bKey;
 }
 
 int *two_sum_mdi(int *arr, int len, int target, int *returnSize)
@@ -100,29 +100,126 @@ int *two_sum_mdi(int *arr, int len, int target, int *returnSize)
 	int *result = NULL;
 	*returnSize = 2;
 	int x = 0;
+	int index = 0;
 	result = calloc(2, sizeof(int));
 
 	for (size_t i = 0; i < len; i++) {
 		x = target - arr[i];
-		printf("x %d arr %d \n", x, arr[i]);
-		if (haskey(x)) {
-			//result[0] = i;
-			//result[1] = 
+		if (haskey(x, &index)) {
+			result[0] = i;
+			result[1] = index;
+			break;
 		}
 		add_hash(arr[i], i);
 	}	
 
 	show_hash();
 
-
 	release_hash();
 	return result;
+}
+#endif
+#define HASH(key, len) (key)*(key)%(len)
+typedef struct hash {
+	int key;
+	int index;
+	struct hash *next;
+} hash;
+
+void show(hash **head, int len)
+{
+	hash *curr = NULL;
+	for (size_t i = 0; i < len; i++) {
+		curr = head[i];
+		while(curr) {
+			printf("key %d ", curr->key);
+			curr = curr->next;
+		}
+	}	
+}
+
+int hasKey(hash **patHash, int len, int key, int *index)
+{
+	int bhit = 0;
+	int hashnum = HASH(key, len);
+	hash *curr = patHash[hashnum];
+	while(curr) {
+		if (curr->key == key) {
+			bhit = 1;
+			*index = curr->index;
+			break;
+		}
+		curr = curr->next;
+	}
+
+	return bhit;
+}
+
+hash *new_node(int key, int index)
+{
+	hash *new = calloc(1, sizeof(hash));
+	new->key = key;
+	new->index = index;
+	new->next = NULL;
+	return new;
+}
+
+void add_hash(hash **patHash, int len, int key, int index)
+{
+	int hashnum = HASH(key, len);
+	hash **curr = &patHash[hashnum];
+	hash *new = new_node(key, index);
+	while(*curr)
+		curr = &(*curr)->next;
+	*curr = new;
+}
+
+void release_hash(hash **head, int len)
+{
+	hash *curr = NULL;
+	hash *remove = NULL;
+	for (size_t i = 0; i < len; i++) {
+		curr = head[i];
+		while(curr) {
+			remove = curr;
+			curr = curr->next;
+			free(remove);
+		}
+	}
+}
+
+int* twosum(int* nums, int numsSize, int target, int* returnSize)
+{
+	int *result = NULL;
+	*returnSize = 2;
+	result = calloc(2, sizeof(int));
+	hash *patHash[numsSize];
+
+	int x = 0;
+	int index = 0;
+	for (size_t i = 0; i < numsSize; i++)
+		patHash[i] = NULL;
+
+	for (size_t i = 0; i < numsSize; i++) {
+		x = target - nums[i];
+		if (hasKey(patHash, numsSize, x, &index)) {
+			result[0] = i;
+			result[1] = index;
+			break;
+		}
+		add_hash(patHash, numsSize, nums[i], i);
+	}
+
+	show(patHash, numsSize);
+	release_hash(patHash, numsSize);
+	return result; 
 }
 
 int main(void)
 {
-	int arr[] = {2, 7, 11, 15};
-	int target = 9;
+	//int arr[] = {2, 7, 11, 15};
+	int arr[] = {3, 2, 4};
+	int target = 6;
 	int returnSize = 0;
 	int len = sizeof(arr)/sizeof(int);
 	int *result = NULL;
@@ -131,7 +228,8 @@ int main(void)
 	for(size_t i = 0; i < returnSize; i++) 
 		printf("%d ", result[i]);
 #endif
-	result = two_sum_mdi(arr, len, target, &returnSize);
+	//	result = two_sum_mdi(arr, len, target, &returnSize);
+	result = twosum(arr, len, target, &returnSize); 
 	for(size_t i = 0; i < returnSize; i++) 
 		printf("%d ", result[i]);
 	printf("\n");
