14161c4cf2b94ebaf5454b585315db9850f3e651
Jadenchen <menzxisland@gmail.com>
2023-01-16 06:24:45 +0800

    cracking: moderate: 16_1

---
+++ cracking_the_code/moderate/16_1/16_1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#define ERROR (65536)
+#define STACK_SIZE (1000)
+#define addr2uint(x) ((uint64_t)(void *)(x))
+#define uint2addr(x) ((void *)(uint64_t)(x))
+
+void swap(int *pa, int *pb)
+{
+	*pa = *pa ^ *pb;
+	*pb = *pb ^ *pa;
+	*pa = *pa ^ *pb;
+}
+
+int main(void)
+{
+	int a = 10;
+	int b = 20;
+
+	//!xor true table
+	// 0 0 0
+	// 1 0 1
+	// 0 1 1
+	// 1 1 0
+
+	printf("ori a %d b %d \n", a, b);
+	swap(&a, &b);
+	printf("mdi a %d b %d \n", a, b);
+	return 0;
+}
