943d2573d27453c523096e2c6b21a8805d6f4fc2
Jadenchen <menzxisland@gmail.com>
2023-01-19 08:47:11 +0800

    cracking: moderate: 16_15

--- cracking_the_code/moderate/16_15/16_15.c
+++ cracking_the_code/moderate/16_15/16_15.c
@@ -16,16 +16,18 @@ char * getHint(char * secret, char * guess)
    char *res = calloc(5, sizeof(char));
    int cnt_A = 0;
    int cnt_B = 0;
+   printf("len %d \n", len);
    for (size_t i = 0; i < len; i++) {
-       cur_s  = secret + i;
+       cur_s = secret + i;
        cur_g = guess;
+
        for(size_t j = 0; j < len; j++) {
-           if (!strcmp(cur_s, cur_g)) {
+	       printf("%c %c \n", cur_s[0], cur_g[j]);
+           if (cur_s[0] == cur_g[j]) {
                if (i == j)
                    cnt_A++;
                else
                    cnt_B++;
-
            }
        }
    }
@@ -35,9 +37,12 @@ char * getHint(char * secret, char * guess)
 
 int main(void)
 {
-	char ser[4] = {'1', '8', '0','7'};
-	char gus[4] = {'7','8','1','0'};
-	printf("%s\n", ser);
-	//printf("%s \n", getHint(ser, gus));	
+//	char ser[5] = {'1', '8', '0', '7', '\0'};
+//	char gus[5] = {'7', '8', '1', '0', '\0'};
+	char ser[5] = {'1', '1', '2', '3', '\0'};
+	char gus[5] = {'0', '1', '1', '1', '\0'};
+
+//	printf("%c%c%c%c\n", ser[0], ser[1], ser[2], ser[3]);
+	printf("%s \n", getHint(ser, gus));	
 	return 0;
 }
