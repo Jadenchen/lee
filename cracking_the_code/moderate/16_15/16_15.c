c16aa12d0c0c760b43adea75954a0926e76242f0
Jaden.Chen <jaden.chen@vatics.com>
2023-01-19 16:08:15 +0800

    cracking: moderate: 16_15

--- cracking_the_code/moderate/16_15/16_15.c
+++ cracking_the_code/moderate/16_15/16_15.c
@@ -10,39 +10,42 @@
 
 char * getHint(char * secret, char * guess)
 {
-   int len = strlen(secret);
-   char *cur_s = NULL;
-   char *cur_g = NULL;
-   char *res = calloc(5, sizeof(char));
-   int cnt_A = 0;
-   int cnt_B = 0;
-   printf("len %d \n", len);
-   for (size_t i = 0; i < len; i++) {
-       cur_s = secret + i;
-       cur_g = guess;
-
-       for(size_t j = 0; j < len; j++) {
-	       printf("%c %c \n", cur_s[0], cur_g[j]);
-           if (cur_s[0] == cur_g[j]) {
-               if (i == j)
-                   cnt_A++;
-               else
-                   cnt_B++;
-           }
-       }
-   }
-    snprintf(res, 5, "%dA%dB", cnt_A, cnt_B);
-    return res;
+	int len = strlen(secret);
+	char *cur_s = NULL;
+	char *cur_g = NULL;
+	char *res = calloc(5, sizeof(char));
+	int cnt_A = 0;
+	int cnt_B = 0;
+	int find = 0;
+	for (size_t i = 0; i < len; i++) {
+		cur_s = secret + i;
+		find = 0;
+		for(size_t j = 0; j < len; j++) {
+			cur_g = guess + j;
+			if (*cur_s == *cur_g) {
+				if (i == j)
+					cnt_A++;
+				else
+					cnt_B++;
+				find = 1;
+			}
+			if (find)
+				break;
+		}
+	}
+	snprintf(res, 5, "%dA%dB", cnt_A, cnt_B);
+	return res;
 }
 
 int main(void)
 {
-//	char ser[5] = {'1', '8', '0', '7', '\0'};
-//	char gus[5] = {'7', '8', '1', '0', '\0'};
-	char ser[5] = {'1', '1', '2', '3', '\0'};
-	char gus[5] = {'0', '1', '1', '1', '\0'};
-
-//	printf("%c%c%c%c\n", ser[0], ser[1], ser[2], ser[3]);
+#if 0
+	char ser[5] = "1807"; 
+	char gus[5] = "7810";
+#else 
+	char ser[5] = "1123";
+	char gus[5] = "0111";
+#endif
 	printf("%s \n", getHint(ser, gus));	
 	return 0;
 }
