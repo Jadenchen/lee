b206a2e70a79b14a9742990ecdc796d19ae848c4
Jadenchen <menzxisland@gmail.com>
2023-01-23 23:07:19 +0800

    cracking: link_list: 2_3

--- cracking_the_code/linked_list/2_3/2_3.c
+++ cracking_the_code/linked_list/2_3/2_3.c
@@ -30,28 +30,64 @@ void del(list *curr, list *remove)
 //! time O(n) space O(1)
 list *removemid(list *head)
 {
-	int len = 0;
-	int index = 0;
-	list *curr = NULL;
+	list *slow = NULL;
+	list *fast = NULL;
 	list *prev = NULL;
+	list *mid = NULL;
 	if (!head || !head->next)
 		return head;
-
-	len = getlen(head);
-	len = len/2;
-	curr = head;
-	while(curr) {
-		if (index++ == index)
-			break;
-		prev = curr;
-		curr = curr->next;
-	}		
-	del(prev, curr);
+	
+	slow = head;
+	fast = head;
+	while(fast && fast->next) {
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	
+	mid = slow;
+	prev = head;
+	while(prev && prev->next != mid) {
+		prev = prev->next;
+	}
+	if (prev->next) 
+		prev->next = mid->next;
+	else 
+		head = head->next;
 	return head;
+}
+
+void init_list(list **head, int val)
+{
+	list **indir = head;
+	list *new = calloc(1, sizeof(list));
+	new->val = val;
+	new->next = NULL;
+	while(*indir) {
+		indir = &(*indir)->next;	
+	}
+	*indir = new;
+}
+void show(list *head)
+{
+	while(head) {
+		printf("%d ", head->val);
+		head = head->next;
+	}
+	printf("\n");
 }
 
 int main(void)
 {
+	int a[4] = {1, 2, 3, 4};
+	int len = sizeof(a)/sizeof(int);
+	list *head = NULL;
+	for (int i = 0; i < len; i++) {
+		init_list(&head, a[i]);
+	}	
+
+	show(head);
+	head = removemid(head);
+	show(head);
 	return 0;
 }
 
