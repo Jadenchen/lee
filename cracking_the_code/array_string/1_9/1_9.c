491cdb38f11e77bb7e81fdd29179c368e2b179f5
Jadenchen <menzxisland@gmail.com>
2023-01-23 11:34:14 +0800

    cracking: array: 1_9

--- cracking_the_code/array_string/1_9/1_9.c
+++ cracking_the_code/array_string/1_9/1_9.c
@@ -2,42 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-int find (int *index, char cmp, char *b, int len)
+int rotateString(char * s, char * goal)
 {
-	int bFind = 0;
-	for (size_t i = 0; i < len; i++) {
-		if (b[i] == cmp) {
-			bFind = 1;
-			*index = i;
-			break;
-		}
-	}
-	return bFind;
-}
-int isRotate(char *a, char *b)
-{
-	int bRotate = 0;
-	char *pa = a;
-	char *pb = b;
-	int b_cnt = 0;
-	int index = 0;
-	int cnt = 0;
-	//! find first 
-	int len = strlen(a);
-	if (find(&index, *pa, b, len)) {
-		for (size_t i = 0; i < len; i++) {
-			pb = &b[(index + i +len)%len];
-			if (*pa == *pb) {
-				cnt++;
-			} 
-			pa++;
-		}
-		if (cnt == len) 
-			bRotate = 1;
-		return bRotate;
-	}  
+    int lens = strlen(s);
+    int leng = strlen(goal);
+    int size = lens*2;
+    char tmp[size];
+    
+    int cnt = 0;
+    if (lens != leng)
+        return 0;
+    //! copy
+    for (int i = 0; i < size; i++) {
+        int index = i % lens;
+        tmp[i] = goal[index];
+    }
 
-	return bRotate;
+    for (int i = 0; i < lens; i++) {
+        if (!strncmp(s, &tmp[i], lens)) {
+            cnt++;
+            break;
+        }
+    }
+    return cnt == 1;
 }
 
 //! time O(n) space O(1)
@@ -45,7 +32,7 @@ int main(void)
 {
 	char a[12] = "waterbottle";
 	char b[12] = "erbottlewat";
-	if (isRotate(a, b)) 
+	if (rotateString(a, b)) 
 		printf("yes\n");
 	else 
 		printf("no\n");	
