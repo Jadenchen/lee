3f4715b28a8c533c1f3e55069cc31d0064b78cda
Jadenchen <menzxisland@gmail.com>
2023-01-24 10:05:08 +0800

    cracking: linked_list: 2_1 (leetcode 83)

--- cracking_the_code/linked_list/2_1/2_1.c
+++ cracking_the_code/linked_list/2_1/2_1.c
@@ -1,18 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct list {
 	int val;
 	struct list *next;
 } list;
 
-
+//! return kth 
 void removedup(list *head)
 {
-	int hasdup = 0;
-	if (!head)
-		return;
+	
 
+}
+
+void init_list(list **head, int val)
+{
+	list **indir = head;
+	list *new = calloc(1, sizeof(list));
+	new->val = val;
+	new->next = NULL;
+	while(*indir) {
+		indir = &(*indir)->next;
+	}
+	*indir = new;
+}
+
+void show(list *head)
+{
+	list *cur = head;
+	while(cur) {
+		printf("%d ", cur->val);
+		cur = cur->next;
+	}	
+	printf("\n");
+}
+
+int get_len(list *head)
+{
+	list *cur = head;
+	int cnt = 0;
+	while(cur) {
+		cnt++;
+		cur = cur->next;
+	}
+	return cnt;
+}
+//! leetcode 83
+list * deleteDuplicates(list* head)
+{
+	list *cur = NULL;
+	list *next = NULL;
+	list *remove = NULL;
+	if (!head || !head->next) 
+		return head;
+	
+	cur = head;
+	while(cur) {
+		next = cur->next;
+		while(next && cur->val == next->val) {
+			remove = next;	
+			cur->next = remove->next;
+			next = remove->next;
+			remove->next = NULL;
+		}
+		cur = next;
+	}
+	return head;
 }
 
 int main(void)
 {
+	int a[3] = {1, 1, 2};
+	int len = sizeof(a)/sizeof(int);
+	list *head = NULL;
+	for (int i = 0; i < len; i++) {
+		init_list(&head, a[i]);
+	}	
+
+	show(head);
+	head = deleteDuplicates(head);
+	show(head);
 	return 0;
 }
