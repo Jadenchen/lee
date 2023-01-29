4c00c6cea3906f9976baf352a129297ca33fab15
Jadenchen <menzxisland@gmail.com>
2023-01-29 20:12:34 +0800

    cracking: moderate: 16_15 (leetcode 299)

--- cracking_the_code/moderate/16_15/16_15.c
+++ cracking_the_code/moderate/16_15/16_15.c
@@ -7,36 +7,57 @@
 #define STACK_SIZE (1000)
 #define addr2uint(x) ((uint64_t)(void *)(x))
 #define uint2addr(x) ((void *)(uint64_t)(x))
+#define min(A, B) (A) > (B) ? (B) : (A)
 
 char * getHint(char * secret, char * guess)
 {
+	char *cura = NULL;
+	char *curb = NULL;
+	int dataA[11] = {0};
+	int dataB[11] = {0};
+	int indexA = 0;
+	int indexB = 0;
 	int len = strlen(secret);
-	char *cur_s = NULL;
-	char *cur_g = NULL;
-	char *res = calloc(5, sizeof(char));
-	int cnt_A = 0;
-	int cnt_B = 0;
-	int find = 0;
-	for (size_t i = 0; i < len; i++) {
-		cur_s = secret + i;
-		find = 0;
-		for(size_t j = 0; j < len; j++) {
-			cur_g = guess + j;
-			if (*cur_s == *cur_g) {
-				if (i == j)
-					cnt_A++;
-				else
-					cnt_B++;
-				find = 1;
-			}
-			if (find)
-				break;
+	int lena = 0;
+	int lenb = 0;
+	char *res = NULL;
+	char *cse = strdup(secret);
+	char *cg = strdup(guess);
+	cura = cse;
+	curb = cg;
+	for (int i = 0; i < len; i++) {
+		if (cura[i] == curb[i]) {
+			lena++;
+			cura[i] = 'A';
+			curb[i] = 'A';
 		}
 	}
-	snprintf(res, 5, "%dA%dB", cnt_A, cnt_B);
-	return res;
-}
 
+	for (int i = 0; i < len; i++) {
+		if (cura[i] == 'A')
+			continue;
+		indexA = cura[i] - '0';
+		dataA[indexA]++;
+	}
+
+	for (int i = 0; i < len; i++) {
+		if (curb[i] == 'A')
+			continue;
+		indexB = curb[i] - '0';
+		dataB[indexB]++;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (dataA[i] && dataB[i]) {
+			int tmp = min(dataA[i], dataB[i]);
+			lenb = lenb + tmp;
+		}
+	}
+
+	res = calloc(20, sizeof(char));
+	snprintf(res, 20, "%dA%dB", lena, lenb);
+	return res; 
+}
 int main(void)
 {
 #if 0
