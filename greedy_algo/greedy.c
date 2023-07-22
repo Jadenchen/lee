dfc66bf4ead8311f749705ada9b053f28ae9ae61
Jadenchen <menzxisland@gmail.com>
2023-07-22 17:45:11 +0800

    leetcode: greedy_algo: Lemonade Change (860)

--- greedy_algo/greedy.c
+++ greedy_algo/greedy.c
@@ -6,27 +6,31 @@
 bool lemonadeChange(int* bills, int billsSize)
 {
 	bool enought = true;
-	const int lemon = 5;
+	//const int lemon = 5;
 	int fivecnts = 0;
+	int tencnts = 0;
 	for (int i = 0; i < billsSize; i++) {
 		if (bills[i] == 5) {
 			fivecnts++;
-		} else {
-			int left = bills[i] - lemon;
-			int j = 0;
-			for (j = 1; j <= fivecnts; j++) {
-				left = left - lemon;
-				if (left == 0) {
-					fivecnts = fivecnts - j;
-					break;
-				}
+		} else if (bills[i] == 10) {
+			if (fivecnts) {
+				fivecnts--;
+				tencnts++;
+			} else {
+				enought = false;
+				break;
 			}
-			if (left) {
+		} else if (bills[i] == 20) {
+			if (tencnts && fivecnts) {
+				tencnts--;
+				fivecnts--;
+			} else if (fivecnts >= 3) {
+				fivecnts = fivecnts - 3;
+			} else {
 				enought = false;
 				break;
 			}
 		}
 	}
-
 	return enought;
 }
--- greedy_algo/main.c
+++ greedy_algo/main.c
@@ -3,12 +3,8 @@
 int main(void)
 {
 	int bills[] = {5,5,5,10,20};
-	int bills1[] = {5,5,10,10,20};
-	
-	printf(" lemonadeChange bill0 %d \n", 
-			lemonadeChange(bills, sizeof(bills)/sizeof(int)));
 
-	printf(" lemonadeChange bill1 %d \n", 
-			lemonadeChange(bills1, sizeof(bills1)/sizeof(int)));
+	printf(" lemonadeChange bill1 %d \n",
+			lemonadeChange(bills, sizeof(bills)/sizeof(int)));
 	return 0;
 }
