d9ce15646d4cb11ce0681fd23617e2b8402104df
Jadenchen <menzxisland@gmail.com>
2024-04-25 07:03:01 +0800

    leetcode: string: 459

--- string/459.c
+++ string/459.c
@@ -6,27 +6,28 @@
 int chk_repeat(char *s, int len, int diff)
 {
 	char cmp[len];
-	memcpy(cmp, s, diff);
 	int totalcnt = len/diff;
 	int cnt = 0;
-	
+	memcpy(cmp, s, diff);
+	if (len % diff != 0)
+		return 0;
+
 	for (int i = 0; i < len; i = i + diff) {
 		if (!strncmp(s + i, cmp, diff)) {
 			cnt++;
 		}
 	}
-
 	return cnt == totalcnt;
 }
 
 bool repeatedSubstringPattern(char* s) {
 	int len = strlen(s);
-	bool result = false;	
+	bool result = false;
 	int maxlen = 0;
-	if (len % 2) 
-		return false;	
+	if (len % 2)
+		return false;
 
-	maxlen = len/2;	
+	maxlen = len/2;
 	for (int i = 0; i < maxlen; i++) {
 		if (chk_repeat(s, len, i + 1)) {
 			result = true;
